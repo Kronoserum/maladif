f14381112393b2158369cb82dd1f6cebcbb0f103
Romain <Romain@DESKTOP-MLAOIJC>
2017-05-16 15:10:48 +0200

    Sockets Serveur

--- Med'IF_Serveur/Med'IF_Serveur/ConnectedSocket.cpp
+++ Med'IF_Serveur/Med'IF_Serveur/ConnectedSocket.cpp
@@ -1,11 +1,14 @@
+// ClientSocket.cpp : fichier d'implémentation
+//
+
 #include "stdafx.h"
 #include "ConnectedSocket.h"
 
+using namespace std;
 
 ConnectedSocket::ConnectedSocket()
 {
 }
-
 
 ConnectedSocket::~ConnectedSocket()
 {
@@ -13,4 +16,28 @@ ConnectedSocket::~ConnectedSocket()
 
 void ConnectedSocket::OnReceive(int nErrorCode)
 {
+	const int BUFF_LEN = 2048;
+	char szBuff[BUFF_LEN];
+
+	int nReceivedSize = Receive(szBuff, BUFF_LEN);
+
+	if (nReceivedSize <= 0)
+		return;
+
+	szBuff[nReceivedSize] = '\0';
+
+	string strResponse = "hello\n";
+
+	int nSentBytes = 0;
+	const char* pszBuff = strResponse.c_str();
+	int nResponseSize = strResponse.length();
+
+	while (nSentBytes < nResponseSize)
+	{
+		nSentBytes = Send(pszBuff, nResponseSize);
+		nResponseSize = nResponseSize - nSentBytes;
+		pszBuff = pszBuff + nSentBytes;
+	}
+
+	CAsyncSocket::OnReceive(nErrorCode);
 }
--- Med'IF_Serveur/Med'IF_Serveur/ListenerSocket.cpp
+++ Med'IF_Serveur/Med'IF_Serveur/ListenerSocket.cpp
@@ -1,11 +1,15 @@
+// ServerListener.cpp : fichier d'implémentation
+//
+
 #include "stdafx.h"
 #include "ListenerSocket.h"
+#include "ConnectedSocket.h"
 
+// ServerListener
 
 ListenerSocket::ListenerSocket()
 {
 }
-
 
 ListenerSocket::~ListenerSocket()
 {
@@ -13,4 +17,15 @@ ListenerSocket::~ListenerSocket()
 
 void ListenerSocket::OnAccept(int nErrorCode)
 {
+	ConnectedSocket* pConnectedSock = new ConnectedSocket();
+
+	CString strPeerName;
+	UINT uiPort;
+
+	if (Accept(*pConnectedSock))
+	{
+		pConnectedSock->GetPeerName(strPeerName, uiPort);
+	}
+
+	CAsyncSocket::OnAccept(nErrorCode);
 }
