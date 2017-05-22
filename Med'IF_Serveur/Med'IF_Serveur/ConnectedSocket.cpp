72a138861b2062384f34c47fffe1d6dcf5ad99bd
Romain <Romain@DESKTOP-MLAOIJC>
2017-05-22 16:01:59 +0200

    Sockets serveur version 1

--- Med'IF_Serveur/Med'IF_Serveur/ConnectedSocket.cpp
+++ Med'IF_Serveur/Med'IF_Serveur/ConnectedSocket.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 ConnectedSocket::ConnectedSocket()
 {
+
 }
 
 ConnectedSocket::~ConnectedSocket()
@@ -16,20 +17,49 @@ ConnectedSocket::~ConnectedSocket()
 
 void ConnectedSocket::OnReceive(int nErrorCode)
 {
+	//--------------------------------------------------------------------Debut de la reception
 	const int BUFF_LEN = 256;
 	char szBuff[BUFF_LEN];
 
 	int nReceivedSize = Receive(szBuff, BUFF_LEN);
-	szBuff[nReceivedSize] = '\0';
-	CString str;
-	AfxMessageBox((CString)szBuff);
+	// Safety check
 	if (nReceivedSize <= 0)
 		return;
-
-	string strResponse = "hello\r\n";
-
+	szBuff[nReceivedSize] = '\0';
 
 
+	//------------------------------------------------------------------------------Traitement
+	//AfxMessageBox((CString)szBuff);
+	string str = string(szBuff);	// commande au format string
+	
+	//CString c (str.c_str());
+	//AfxMessageBox(c);
+
+	string strResponse;
+	if (str.find(":") == string::npos)
+	{
+		strResponse = "Erreur : format de commande incorrect.\r\n";
+	}
+	else {
+		string requete = str.substr(0, str.find(":"));
+		CString c(requete.c_str());
+		//AfxMessageBox(c);
+		if (requete.compare("disconnect") == 0)
+		{
+			//AfxMessageBox(_T("J'ai recu le message bonjour"));
+			//strResponse = "Bonjour a toi !\r\n";
+			AfxMessageBox(_T("Déconnexion..."));
+			this->Close();
+		}
+		else
+		{	
+			AfxMessageBox(_T("Je ne connais pas cette requete"));
+			strResponse = "hello\r\n";
+		}
+	}
+	
+
+	//---------------------------------------------------------------------Envoi de la réponse
 	int nSentBytes = 0;
 	const char* pszBuff = strResponse.c_str();
 	int nResponseSize = strResponse.length();
